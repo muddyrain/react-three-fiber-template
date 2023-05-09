7fb6c0167cc407e27ce3074167a78525060f0682
“muddyrain” <qiu3291002845@gmail.com>
2023-05-09 16:44:40 +0800

    feat(custom): vr-全景房-添加精灵图切换房间

--- src/pages/vr-house/index.tsx
+++ src/pages/vr-house/index.tsx
@@ -1,18 +1,61 @@
 import { STATIC_SERVER_URL } from '@/constant'
-import { FC, forwardRef, useEffect, useRef, useState } from 'react'
+import { FC, Suspense, forwardRef, useEffect, useRef, useState } from 'react'
 import mapGif from './image/map.gif'
 import locationImg from './image/location.png'
 import * as THREE from 'three'
-import { Canvas, Euler, Vector3, useLoader, useThree } from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
+import { Canvas, Euler, ThreeEvent, Vector3, useFrame, useLoader, useThree } from '@react-three/fiber'
+import { Environment, OrbitControls, Text, Text3D } from '@react-three/drei'
 import { TextureLoader } from 'three'
 import { useMouseMove } from './useMouseMove'
+import gsap from 'gsap'
+const Sprite: FC<{
+  name: string
+  position: Vector3
+  onClick: (event: ThreeEvent<MouseEvent>) => void
+}> = ({ name, position = [0, 0, 0], onClick }) => {
+  const spriteRef = useRef<THREE.Sprite>(null)
+  useFrame(() => {
+    if (spriteRef.current) {
+      const canvas = document.createElement('canvas')
+      canvas.width = 1024
+      canvas.height = 1024
+      const context = canvas.getContext('2d') as CanvasRenderingContext2D
+      canvas.style.cursor = 'pointer'
+      context.fillStyle = 'rgba(100,100,100,0.7)'
+      context.fillRect(0, 256, 1024, 512)
+      context.textAlign = 'center'
+      context.textBaseline = 'middle'
+      context.font = 'bold 200px Arial'
+      context.fillStyle = 'white'
+      context.fillText(name, 512, 512)
+      const texture = new THREE.CanvasTexture(canvas)
+      spriteRef.current.material.map = texture
+      spriteRef.current.material.needsUpdate = true
+      spriteRef.current.scale.set(1, 1, 1)
+    }
+  })
+  return (
+    <>
+      <sprite
+        onClick={(e) => {
+          onClick(e)
+        }}
+        position={position}
+        ref={spriteRef}
+      >
+        <spriteMaterial />
+      </sprite>
+    </>
+  )
+}
 const Room: FC<{
+  name?: string
   roomIndex: number
   textureUrl: string
   position: Vector3
   euler: Euler
-}> = ({ position, euler, roomIndex, textureUrl }) => {
+  sprites?: React.ReactNode
+}> = ({ name, position, euler, roomIndex, textureUrl, sprites }) => {
   const [materials, setMaterials] = useState<THREE.MeshBasicMaterial[]>([])
   const boxGeometry = useRef<THREE.BoxGeometry>(new THREE.BoxGeometry(10, 10, 10))
   boxGeometry.current.scale(1, 1, -1)
@@ -34,7 +77,13 @@ const Room: FC<{
       materials.push(new THREE.MeshBasicMaterial({ map: texture }))
     })
   }, [])
-  return <mesh position={position} rotation={euler} args={[boxGeometry.current, materials]} />
+
+  return (
+    <>
+      <mesh position={position} rotation={euler} args={[boxGeometry.current, materials]} />
+      {sprites}
+    </>
+  )
 }
 const WatchMove = ({ container }) => {
   const { camera } = useThree()
@@ -48,12 +97,36 @@ const Fragment: FC = () => {
   return (
     <div className='w-full h-full relative' ref={containerRef}>
       {/* camera={{ position: [0, 0, 0], far: 1000, near: 0.1, fov: 75 }} */}
-      <Canvas gl={{ preserveDrawingBuffer: true }} camera={{ position: [0, 0, 0], far: 1000, near: 0.1, fov: 75 }}>
-        <Room roomIndex={0} textureUrl={'livingroom'} position={[0, 0, 0]} euler={[0, -Math.PI / 2, 0]} />
-        <Room roomIndex={3} textureUrl={'kitchen'} position={[10, 0, -5]} euler={[0, -Math.PI, 0]} />
-        <Room roomIndex={8} textureUrl={'balcony'} position={[-10, 0, 0]} euler={[0, Math.PI / 2, 0]} />
-        <WatchMove container={containerRef} />
-        {/* <OrbitControls /> */}
+      <Canvas gl={{ preserveDrawingBuffer: true }} camera={{ position: [0, 0, 5], far: 1000, near: 0.1, fov: 75 }}>
+        <Suspense fallback={null}>
+          <Room
+            name='客厅'
+            roomIndex={0}
+            textureUrl={'livingroom'}
+            position={[0, 0, 0]}
+            euler={[0, -Math.PI / 2, 0]}
+            sprites={
+              <>
+                <Sprite
+                  onClick={(e) => {
+                    gsap.to(e.camera.position, {
+                      duration: 1,
+                      x: 10,
+                      y: 0,
+                      z: -5
+                    })
+                  }}
+                  name='厨房'
+                  position={[4, 0, -1.5]}
+                />
+              </>
+            }
+          />
+          <Room roomIndex={3} textureUrl={'kitchen'} position={[10, 0, -5]} euler={[0, -Math.PI, 0]} />
+          <Room roomIndex={8} textureUrl={'balcony'} position={[-10, 0, 0]} euler={[0, Math.PI / 2, 0]} />
+          {/* <OrbitControls /> */}
+          <WatchMove container={containerRef} />
+        </Suspense>
       </Canvas>
       <div className='w-[300px] h-[260px] absolute left-0 bottom-0'>
         <div className='absolute w-[30px] h-[30px] tag' ref={tagRef}>
